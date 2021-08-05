8c5ffe0280faa39e1e842ac67e0bc4bd8c87923a
Daniel Luiz <danielluizthiessen@gmail.com>
2021-08-05 17:43:52 -0300

    Heroku

--- src/entities/Period.ts
+++ src/entities/Period.ts
@@ -1,6 +1,6 @@
 import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
 
-@Entity("categories")
+@Entity("periods")
 export default class Category {
   @PrimaryGeneratedColumn()
   id: number;
